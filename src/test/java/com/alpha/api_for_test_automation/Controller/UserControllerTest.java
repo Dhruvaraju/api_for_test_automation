6c7fd2dba8fa245bb61376c4ec1aaabe4e476e8c
Dhruva Raju <49dhruva@gmail.com>
2022-07-28 21:15:59 +0530

    feat(test): base unit test files added

---
+++ src/test/java/com/alpha/api_for_test_automation/Controller/UserControllerTest.java
@@ -0,0 +1,34 @@
+package com.alpha.api_for_test_automation.Controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void registerUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void removeUser() {
+    }
+}
---
+++ src/test/java/com/alpha/api_for_test_automation/Service/UserServiceTest.java
@@ -0,0 +1,30 @@
+package com.alpha.api_for_test_automation.Service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addOrUpdateUser() {
+    }
+
+    @Test
+    void fetchAllUsers() {
+    }
+
+    @Test
+    void removeUser() {
+    }
+}
