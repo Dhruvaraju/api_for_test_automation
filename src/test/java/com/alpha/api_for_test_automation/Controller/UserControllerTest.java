59d4e9d2855e68ddc891419316dfc20455b7b4f6
Dhruva Raju <49dhruva@gmail.com>
2022-07-29 21:19:34 +0530

    feat(unit-tests): added controller and service tests

--- src/test/java/com/alpha/api_for_test_automation/Controller/UserControllerTest.java
+++ src/test/java/com/alpha/api_for_test_automation/Controller/UserControllerTest.java
@@ -1,23 +1,72 @@
 package com.alpha.api_for_test_automation.Controller;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import com.alpha.api_for_test_automation.Entity.User;
+import com.alpha.api_for_test_automation.Service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(MockitoExtension.class)
 class UserControllerTest {
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private UserController userController;
+    private User requestUser;
+    private User responseUser;
+    private List<User> userList = new ArrayList<>();
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
-    }
+        requestUser = new User();
+        requestUser.setUserName("Alpha");
+        requestUser.setEmail("alpha@alpha.com");
+        requestUser.setPassword("Ab43R5ce");
 
-    @AfterEach
-    void tearDown() {
+        responseUser = new User(1, "Alpha", "alpha@alpha.com", "Ab43R5ce");
+        userList.add(responseUser);
+        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
     }
 
     @Test
-    void getAllUsers() {
+    @Tag("happy-path")
+    @DisplayName("Should get list of all users.")
+    void getAllUsers() throws Exception {
+        when(userService.fetchAllUsers()).thenReturn(userList);
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(requestUser)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].userName").value("Alpha"))
+                .andExpect(jsonPath("$[0].email").value("alpha@alpha.com"));
+        verify(userService, times(1)).fetchAllUsers();
     }
 
     @Test
@@ -30,5 +79,13 @@ class UserControllerTest {
 
     @Test
     void removeUser() {
+    }
+
+    private static String asJsonString(final Object obj){
+        try{
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/test/java/com/alpha/api_for_test_automation/Service/UserServiceTest.java
+++ src/test/java/com/alpha/api_for_test_automation/Service/UserServiceTest.java
@@ -1,30 +1,69 @@
 package com.alpha.api_for_test_automation.Service;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import com.alpha.api_for_test_automation.Entity.User;
+import com.alpha.api_for_test_automation.Repository.UserRepository;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+    private User requestUser;
+    private User responseUser;
+    private List<User> userList = new ArrayList<>();
 
     @BeforeEach
     void setUp() {
-    }
+        requestUser = new User();
+        requestUser.setUserName("Alpha");
+        requestUser.setEmail("alpha@alpha.com");
+        requestUser.setPassword("Ab43R5ce");
 
-    @AfterEach
-    void tearDown() {
+        responseUser = new User(1, "Alpha", "alpha@alpha.com", "Ab43R5ce");
+        userList.add(responseUser);
     }
 
     @Test
+    @Tag("happy-path")
+    @DisplayName("Should add a new user and return it.")
     void addOrUpdateUser() {
+        when(userRepository.save(any())).thenReturn(responseUser);
+        User actualResult = userService.addOrUpdateUser(requestUser);
+        assertEquals("Alpha", actualResult.getUserName());
+        verify(userRepository, times(1)).save(requestUser);
     }
 
     @Test
+    @Tag("happy-path")
+    @DisplayName("Should return all the entities available from database")
     void fetchAllUsers() {
+        when(userRepository.findAll()).thenReturn(userList);
+        List<User> actualResult = userService.fetchAllUsers();
+        assertEquals(1, actualResult.size());
+        verify(userRepository, times(1)).findAll();
     }
 
     @Test
+    @Tag("happy-path")
+    @DisplayName("Should remove a user")
     void removeUser() {
+        doNothing().when(userRepository).delete(requestUser);
+        userService.removeUser(requestUser);
+        verify(userRepository, times(1)).delete(requestUser);
     }
 }
